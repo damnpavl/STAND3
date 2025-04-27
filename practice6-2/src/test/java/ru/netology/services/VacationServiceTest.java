4a9953e2104f0b303be8b8627303bec90f90a0dc
damnpavl <damnpavl0v@icloud.com>
2025-04-27 23:34:17 +0300

    Add files via upload

---
+++ practice6-2/src/test/java/ru/netology/services/VacationServiceTest.java
@@ -0,0 +1,16 @@
+package ru.netology.services;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VacationServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testdata.csv", numLinesToSkip = 1)
+    void shouldCalculateVacationMonths(int income, int expenses, int threshold, int expected) {
+        VacationService service = new VacationService();
+        int actual = service.calculate(income, expenses, threshold);
+        assertEquals(expected, actual);
+    }
+}
