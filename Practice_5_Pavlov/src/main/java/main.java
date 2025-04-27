5c10fa38b243e4dea2b2a94a62d4b2f0a5ca7511
damnpavl <damnpavl0v@icloud.com>
2025-04-27 19:28:35 +0300

    Add files via upload

---
+++ Practice_5_Pavlov/src/main/java/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long expected = 30;
+        long actual = service.calculate(1000, true);
+        System.out.println("1. " + expected + " == ? == " + actual);
+
+        expected = 500;
+        actual = service.calculate(1_000_000, true);
+        System.out.println("2. " + expected + " == ? == " + actual);
+
+        expected = 10;
+        actual = service.calculate(1000, false);
+        System.out.println("3. " + expected + " == ? == " + actual);
+
+        expected = 500;
+        actual = service.calculate(1_000_000, false);
+        System.out.println("4. " + expected + " == ? == " + actual);
+
+    }
+}
